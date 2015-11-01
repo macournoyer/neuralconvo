7275434d183e1dcd3452a9a6d8ba87e4eebbbc4e
macournoyer <macournoyer@gmail.com>
2015-11-01 15:09:43 -0500

    Sandboxing

--- sandbox.lua
+++ sandbox.lua
@@ -1,12 +1,12 @@
 require 'e'
 
-local loader = e.CornellMovieDialogs("data/cornell_movie_dialogs")
+-- local loader = e.CornellMovieDialogs("data/cornell_movie_dialogs")
 -- local rawDataset = loader:load()
-local dataset = e.DataSet("data/cornell_movie_dialogs.t7", loader)
+local dataset = e.DataSet("data/cornell_movie_dialogs.t7", e.CornellMovieDialogs("data/cornell_movie_dialogs"))
 
 -- local processor = e.PreProcessor()
 -- processor:visit(rawDataset)
 -- local dataset = processor.data
 
-print(#dataset.data .. " examples")
+print(#dataset.examples .. " examples")
 print(dataset.vocabSize .. " words")
